87f3839caa3d7a769f9669f5655501f997bbefb2
floreal15 <floreal.sangenis@algoo.fr>
2024-11-07 11:40:33 +0100

    fix(front): revert some commit

--- angular-gtt-webapp/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api',  // Your backend API URL
-  authentikUrl: 'https://your-authentik-domain',  // Authentik server URL
-  clientId: 'your-client-id',  // OAuth client ID from Authentik
-};
