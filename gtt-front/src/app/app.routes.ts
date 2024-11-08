5e9c823342fb90695e831e522e09e74eadef683b
Seynabou <seynabousconde@gmail.com>
2024-11-08 14:11:41 +0100

    create home page

--- gtt-front/src/app/app.routes.ts
+++ gtt-front/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent }  from '../home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'accueil', component: HomeComponent },
+    { path: '', redirectTo: '/accueil', pathMatch: 'full' }
+];
