52abf932b1e13d603465d933169f206d8e56bca9
najouss08 <l.najla1999@hotmail.fr>
2025-02-05 08:28:46 +0100

    munu modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,8 +8,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomeComponent {
   currentTitle: string = 'Saisie des temps';
+  activePage: string = 'saisie';
 
-  
 constructor(
   private readonly route: ActivatedRoute,
   private readonly router: Router  ) {}
@@ -22,10 +22,30 @@ constructor(
 
     this.router.navigate(['encodedTitle'], {relativeTo: this.route})
     .then(nav => {
-      console.log(nav , "true if navigation is successful"); 
+      console.log(nav , "true if navigation is successful");
     }, err => {
       console.log(err, "when there's an error");
     });
+  }
+  showPage(page: string) {
+    this.activePage = page;
+
+    switch (page) {
+      case 'saisie':
+        this.currentTitle = 'Saisie du temps';
+        break;
+      case 'projets':
+        this.currentTitle = 'Liste des projets';
+        break;
+      case 'frais':
+        this.currentTitle = 'Liste Frais de déplacement';
+        break;
+      case 'download':
+        this.currentTitle = 'Téléchargement';
+        break;
+      default:
+        this.currentTitle = 'Saisie du temps';
+    }
   }
 
 }
