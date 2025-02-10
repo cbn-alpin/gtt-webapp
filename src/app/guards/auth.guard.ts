59078efaa6b67a09af4befc3aa464adc4a721b58
MATIAS FREUND GALEANO <matiasfreund@gmail.com>
2025-02-10 08:50:48 +0100

    feat(auth): moved auth logic to frontend

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+// src/app/guards/auth.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    } else {
+      // Navigate to the login/connection page if not authenticated
+      this.router.navigate(['/connexion']);
+      return false;
+    }
+  }
+}
