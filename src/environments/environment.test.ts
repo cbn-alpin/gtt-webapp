1ee3a12ce5fc5b2186c2968d91f993e60a6e4c3f
MATIAS FREUND GALEANO <matiasfreund@gmail.com>
2025-03-13 13:22:33 +0100

    Merge branch 'auth' into develop

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://127.0.0.1:5000/api',
+    googleClientId:'800152835915-atf9657e73dip71f7velahqvn3rhf1k0.apps.googleusercontent.com'
+};
