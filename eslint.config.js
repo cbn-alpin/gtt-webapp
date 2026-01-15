8052ff7d7003ea3f3f982e261ca2afcefe0651bc
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-01-15 11:39:54 +0100

    chore(eslint): add rule to enforce kebab-case notation for filenames

--- eslint.config.js
+++ eslint.config.js
@@ -4,41 +4,56 @@ const { defineConfig } = require("eslint/config");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
-module.exports = defineConfig([
-  {
-    files: ["**/*.ts"],
-    extends: [
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
-    ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
+// Using an async IIFE (Immediately Invoked Function Expression) to use dynamic import
+module.exports = (async () => {
+  const unicorn = await import("eslint-plugin-unicorn");
+
+  return defineConfig([
+    {
+      files: ["**/*.ts"],
+      extends: [
+        eslint.configs.recommended,
+        tseslint.configs.recommended,
+        tseslint.configs.stylistic,
+        angular.configs.tsRecommended,
       ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
+      plugins: {
+        unicorn: unicorn.default,
+      },
+      processor: angular.processInlineTemplates,
+      rules: {
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
+        ],
+        "unicorn/filename-case": [
+          "error",
+          {
+            "case": "kebabCase",
+            "ignore": [/^.*\.config\.js$/]
+          }
+        ]
+      },
     },
-  },
-  {
-    files: ["**/*.html"],
-    extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
-    ],
-    rules: {},
-  }
-]);
+    {
+      files: ["**/*.html"],
+      extends: [
+        angular.configs.templateRecommended,
+        angular.configs.templateAccessibility,
+      ],
+      rules: {},
+    }
+  ]);
+})();
