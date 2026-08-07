af06182dbb1d1eefc651779b0dc53d830a026c06
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-08-07 12:30:35 +0200

    chore: add eslint compact format

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const { defineConfig } = require("eslint/config");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
 
 // Using an async IIFE (Immediately Invoked Function Expression) to use dynamic import
 module.exports = (async () => {
-  const unicorn = await import("eslint-plugin-unicorn");
+  const unicorn = await import('eslint-plugin-unicorn');
 
   return defineConfig([
     {
-      files: ["**/*.ts"],
+      files: ['**/*.ts'],
       extends: [
         eslint.configs.recommended,
         tseslint.configs.recommended,
@@ -22,56 +22,52 @@ module.exports = (async () => {
       },
       processor: angular.processInlineTemplates,
       rules: {
-        "@angular-eslint/directive-selector": [
-          "error",
+        '@angular-eslint/directive-selector': [
+          'error',
           {
-            type: "attribute",
-            prefix: "app",
-            style: "camelCase",
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase',
           },
         ],
-        "@angular-eslint/component-selector": [
-          "warn",
+        '@angular-eslint/component-selector': [
+          'error',
           {
-            type: "element",
-            prefix: "app",
-            style: "kebab-case",
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case',
           },
         ],
         // ⚠️ Temporarily set to "warn" to unblock CI
         // TODO: 👷🏼 Fix all occurrences category by category, then restore the "error" value
-        "@angular-eslint/prefer-inject": "warn",
-        "unicorn/filename-case": [
-          "warn",
+        '@angular-eslint/prefer-inject': 'error',
+        'unicorn/filename-case': [
+          'error',
           {
-            "case": "kebabCase",
-            "ignore": [/^.*\.config\.js$/]
-          }
+            case: 'kebabCase',
+            ignore: [/^.*\.config\.js$/],
+          },
         ],
-        "@typescript-eslint/no-unused-vars": "warn",
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@angular-eslint/contextual-lifecycle": "warn",
-        "@typescript-eslint/no-unused-expressions": "warn",
-        "no-prototype-builtins": "warn",
-
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@angular-eslint/contextual-lifecycle': 'warn',
+        '@typescript-eslint/no-unused-expressions': 'warn',
+        'no-prototype-builtins': 'warn',
       },
     },
     {
-      files: ["**/*.html"],
-      extends: [
-        angular.configs.templateRecommended,
-        angular.configs.templateAccessibility,
-      ],
+      files: ['**/*.html'],
+      extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
       rules: {
         // ⚠️ Temporarily set to "warn" to unblock CI
         // TODO: 👷🏼 Fix all occurrences category by category, then restore the "error" value
-        "@angular-eslint/template/eqeqeq": "warn",
-        "@angular-eslint/template/click-events-have-key-events": "warn",
-        "@angular-eslint/template/interactive-supports-focus": "warn",
-        "@angular-eslint/template/alt-text": "warn",
+        '@angular-eslint/template/eqeqeq': 'warn',
+        '@angular-eslint/template/click-events-have-key-events': 'warn',
+        '@angular-eslint/template/interactive-supports-focus': 'warn',
+        '@angular-eslint/template/alt-text': 'warn',
         // ⚠️ Rule is for Angular 17+ control flow. Not relevant for v15.
-        "@angular-eslint/template/prefer-control-flow": "off",
+        '@angular-eslint/template/prefer-control-flow': 'off',
       },
-    }
+    },
   ]);
 })();
