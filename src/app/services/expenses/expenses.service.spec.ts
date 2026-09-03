199a46e65f98be55ad12010a33ba03716eeade28
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 21:55:01 +0200

    tests(expenses.service): fix unit tests

--- src/app/services/expenses/expenses.service.spec.ts
+++ src/app/services/expenses/expenses.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ExpensesService } from './expenses.service';
@@ -6,7 +7,9 @@ describe('ExpensesService', () => {
   let service: ExpensesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ExpensesService);
   });
 
