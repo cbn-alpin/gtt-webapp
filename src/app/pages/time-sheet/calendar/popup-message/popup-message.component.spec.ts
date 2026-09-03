66ed1615af8087dcc4f78393d25a6e235ca5362a
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 21:05:11 +0200

    tests(popup-message): fix unit tests

--- src/app/pages/time-sheet/calendar/popup-message/popup-message.component.spec.ts
+++ src/app/pages/time-sheet/calendar/popup-message/popup-message.component.spec.ts
@@ -1,4 +1,6 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { PopupMessageComponent } from './popup-message.component';
 
@@ -6,9 +8,13 @@ describe('PopupMessageComponent', () => {
   let component: PopupMessageComponent;
   let fixture: ComponentFixture<PopupMessageComponent>;
 
+  const mockData = { title: 'One title', message: 'One long message.' };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PopupMessageComponent],
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: mockData }],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PopupMessageComponent);
