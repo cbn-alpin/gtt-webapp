759d5a786f14ad585cfeac2db74090d4bfd740ae
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 18:23:43 +0200

    tests(project-actions): fix unit tests

--- src/app/pages/list-projects/project-actions/project-actions.component.spec.ts
+++ src/app/pages/list-projects/project-actions/project-actions.component.spec.ts
@@ -1,14 +1,48 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { of } from 'rxjs';
+
+import { ProjectActionsService } from 'src/app/services/project-actions/project-actions.service';
+import { ProjectsService } from 'src/app/services/projects/projects.service';
+import { UserActionService } from 'src/app/services/user-action/user-action.service';
 import { ProjectActionsComponent } from './project-actions.component';
 
 describe('ProjectActionsComponent', () => {
   let component: ProjectActionsComponent;
   let fixture: ComponentFixture<ProjectActionsComponent>;
 
+  const mockUserActionService = {
+    createUserAction: jasmine.createSpy('createUserAction'),
+    deleteUserAction: jasmine.createSpy('deleteUserAction'),
+  };
+  const mockProjectService = {
+    getProjectById: jasmine.createSpy('getProjectById').and.returnValue(of([])),
+  };
+  const mockProjectActionService = {
+    getUserProjects: jasmine.createSpy('getUserProjects'),
+    deleteActionById: jasmine.createSpy('deleteActionById'),
+  };
+  const mockDialog = {
+    close: jasmine.createSpy('close'),
+  };
+  const mockSnackBar = {
+    open: jasmine.createSpy('open'),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProjectActionsComponent],
+      providers: [
+        { provide: UserActionService, useValue: mockUserActionService },
+        { provide: ProjectsService, useValue: mockProjectService },
+        { provide: ProjectActionsService, useValue: mockProjectActionService },
+        { provide: MatDialog, useValue: mockDialog },
+        { provide: MatSnackBar, useValue: mockSnackBar },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectActionsComponent);
