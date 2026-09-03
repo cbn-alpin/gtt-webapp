b5abd498edab46d10ff0e2dd37c4d0867a898cf2
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 15:30:04 +0200

    tests(auth.service): fix and add unit tests

--- src/app/core/auth/auth.service.spec.ts
+++ src/app/core/auth/auth.service.spec.ts
@@ -1,16 +1,41 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
+
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate'),
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthService, { provide: Router, useValue: mockRouter }],
+    });
+
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
+    localStorage.clear();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should logout, clear storage and navigate to /connexion', () => {
+    localStorage.setItem('access_token', 'fake-token');
+    service.logout();
+    expect(localStorage.getItem('access_token')).toBeNull();
+    expect(service.token).toBeNull();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/connexion']);
   });
 });
