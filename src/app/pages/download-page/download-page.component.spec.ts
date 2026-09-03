d79db39912dedb12e0015f663fd50e1cc4a885ff
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 16:37:04 +0200

    tests(download-page): fix and add unit tests

--- src/app/pages/download-page/download-page.component.spec.ts
+++ src/app/pages/download-page/download-page.component.spec.ts
@@ -1,6 +1,14 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DownloadPageComponent } from './download-page.component';
+
+@Component({ selector: 'app-download-projects', template: '' })
+class MockDownloadProjectsComponent {}
+
+@Component({ selector: 'app-download-expenses', template: '' })
+class MockDownloadExpensesComponent {}
 
 describe('DownloadPageComponent', () => {
   let component: DownloadPageComponent;
@@ -8,7 +16,12 @@ describe('DownloadPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DownloadPageComponent],
+      declarations: [
+        DownloadPageComponent,
+        MockDownloadProjectsComponent,
+        MockDownloadExpensesComponent,
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DownloadPageComponent);
@@ -18,5 +31,10 @@ describe('DownloadPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain download-projects and download-expenses', () => {
+    expect(fixture.debugElement.query(By.directive(MockDownloadProjectsComponent))).not.toBeNull();
+    expect(fixture.debugElement.query(By.directive(MockDownloadExpensesComponent))).not.toBeNull();
   });
 });
