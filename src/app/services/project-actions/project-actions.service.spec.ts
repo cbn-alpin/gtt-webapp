efac58388e6475b362c38f824cc2d660a136cdaa
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 21:58:25 +0200

    tests(project-actions.service): fix unit tests

--- src/app/services/project-actions/project-actions.service.spec.ts
+++ src/app/services/project-actions/project-actions.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ProjectActionsService } from './project-actions.service';
@@ -6,7 +7,9 @@ describe('ProjectActionsService', () => {
   let service: ProjectActionsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ProjectActionsService);
   });
 
