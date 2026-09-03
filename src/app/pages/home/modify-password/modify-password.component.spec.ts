5eeb90d77a2196b377efa432f3ef4292220fc0f5
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 17:06:24 +0200

    tests(modify-password): fix unit tests

--- src/app/pages/home/modify-password/modify-password.component.spec.ts
+++ src/app/pages/home/modify-password/modify-password.component.spec.ts
@@ -1,14 +1,29 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { UserService } from 'src/app/services/user/user.service';
 import { ModifyPasswordComponent } from './modify-password.component';
 
 describe('ModifyPasswordComponent', () => {
   let component: ModifyPasswordComponent;
   let fixture: ComponentFixture<ModifyPasswordComponent>;
 
+  const mockUserService = {
+    updateUserById: jasmine.createSpy('updateUserById'),
+  };
+  const mockSnackBar = {
+    open: jasmine.createSpy('open'),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ModifyPasswordComponent],
+      providers: [
+        { provide: UserService, useValue: mockUserService },
+        { provide: MatSnackBar, useValue: mockSnackBar },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ModifyPasswordComponent);
