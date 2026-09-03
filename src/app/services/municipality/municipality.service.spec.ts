37fae4ccab530283e0c1dc1ff55092acbcd0a7b7
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 21:55:49 +0200

    tests(municipality.service): fix unit tests

--- src/app/services/municipality/municipality.service.spec.ts
+++ src/app/services/municipality/municipality.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { MunicipalityService } from './municipality.service';
@@ -6,7 +7,9 @@ describe('MunicipalityService', () => {
   let service: MunicipalityService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(MunicipalityService);
   });
 
