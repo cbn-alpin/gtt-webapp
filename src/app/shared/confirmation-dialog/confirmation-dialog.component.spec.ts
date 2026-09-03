4ffba44dd328677f9deea8f0059c5434b60ef4de
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-09-03 22:07:45 +0200

    tests(confirmation-dialog): fix unit tests

--- src/app/shared/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/shared/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,4 +1,6 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
@@ -6,9 +8,19 @@ describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
   let fixture: ComponentFixture<ConfirmationDialogComponent>;
 
+  const mockDialogRef = {
+    close: jasmine.createSpy('close'),
+  };
+  const mockData = { message: 'A long message.' };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ConfirmationDialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: mockData },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationDialogComponent);
